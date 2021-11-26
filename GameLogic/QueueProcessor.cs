15c6235f7cf3054221f5d99b79b1b28883b80fe9
Kinsi <kinsi55@users.noreply.github.com>
2021-11-26 09:23:03 +0100

    Always seed the RNG

--- GameLogic/QueueProcessor.cs
+++ GameLogic/QueueProcessor.cs
@@ -35,6 +35,9 @@ namespace Shaffuru.GameLogic {
 			//songQueueManager.EnqueueSong("custom_level_64B81F99B76742B3C70EAC3BDA4230ED55E0D2AD", BeatmapDifficulty.Hard, 50, 4);
 			//songQueueManager.EnqueueSong("custom_level_D77B25882287CD6CDF4E6D784BBE607C3295F79A", BeatmapDifficulty.Hard, 30, 5.5f);
 			//songQueueManager.EnqueueSong("custom_level_9F783CE7F810062852795F4CBDF8335245FD044A", BeatmapDifficulty.ExpertPlus, 137);
+
+			// Basegame always Initializes the RNG with seed 0 on scene change.. That would be kinda not very RNG probably maybe. Cant hurt.
+			UnityEngine.Random.InitState((int)DateTime.Now.Ticks);
 		}
 
 		public float switchToNextBeatmapAt = 1f;
