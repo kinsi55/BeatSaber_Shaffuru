576b55038ad2cc20b41990bcd03f6a27095a94e7
Kinsi <kinsi55@users.noreply.github.com>
2022-07-13 06:01:56 +0200

    Optimize mock beatmap creator

--- MenuLogic/Anlasser.cs
+++ MenuLogic/Anlasser.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 
 namespace Shaffuru.MenuLogic {
 	class Anlasser {
-		public const string LevelId = "___Shaffuru";
+		public const string LevelIdPrefix = "___Shaffuru_";
 
 		readonly PlayerDataModel playerDataModel;
 		readonly MenuTransitionsHelper menuTransitionsHelper;
 
-		static BeatmapLevelSO beatmapLevel;
-		static BeatmapDataSO beatmapLevelData;
-		static BeatmapLevelSO.DifficultyBeatmap difficultyBeatmap;
+
+		static int lastShaffuruMapLength = 0;
+		public readonly static BeatmapLevelSO beatmapLevel = ScriptableObject.CreateInstance<BeatmapLevelSO>();
+		readonly static BeatmapDataSO beatmapLevelData = ScriptableObject.CreateInstance<BeatmapDataSO>();
+		public readonly static BeatmapLevelSO.DifficultyBeatmap difficultyBeatmap = new BeatmapLevelSO.DifficultyBeatmap(beatmapLevel, BeatmapDifficulty.ExpertPlus, 0, 10, 0, beatmapLevelData);
+		
 		static UBinder<Plugin, System.Random> rngSource;
 
 
@@ -29,10 +32,6 @@ namespace Shaffuru.MenuLogic {
 			this.playerDataModel = playerDataModel;
 			this.menuTransitionsHelper = menuTransitionsHelper;
 
-			beatmapLevel ??= ScriptableObject.CreateInstance<BeatmapLevelSO>();
-			beatmapLevelData ??= ScriptableObject.CreateInstance<BeatmapDataSO>();
-			difficultyBeatmap ??= new BeatmapLevelSO.DifficultyBeatmap(beatmapLevel, BeatmapDifficulty.ExpertPlus, 0, 10, 0, beatmapLevelData);
-
 			rngSource = rng;
 
 			defaultEnvironment ??= customLevelLoader.LoadEnvironmentInfo("", false);
@@ -41,16 +40,37 @@ namespace Shaffuru.MenuLogic {
 
 		public event Action<LevelCompletionResults> finishedOrFailedCallback;
 
-		public void Start(int lengthSeconds, int rngSeed = 0) {
-			beatmapLevel.beatmapLevelData.audioClip?.UnloadAudioData();
 
-			var audioClip = AudioClip.Create("testSound", 1000 * lengthSeconds, 1, 1000, false);
-			// I SetData() an empty float array of matching size before but apparently thats not necessary
+		static readonly IPA.Utilities.FieldAccessor<BeatmapLevelSO, AudioClip>.Accessor BeatmapLevelSO_audioClip =
+			IPA.Utilities.FieldAccessor<BeatmapLevelSO, AudioClip>.GetAccessor("_audioClip");
 
-			if(rngSeed != 0)
-				rngSource.Value = new System.Random(rngSeed);
+		static readonly IPA.Utilities.FieldAccessor<BeatmapLevelSO, string>.Accessor BeatmapLevelSO_songName =
+			IPA.Utilities.FieldAccessor<BeatmapLevelSO, string>.GetAccessor("_songName");
 
-			var bpm = 13.37f;
+		static readonly IPA.Utilities.FieldAccessor<BeatmapLevelSO, string>.Accessor BeatmapLevelSO_levelID =
+			IPA.Utilities.FieldAccessor<BeatmapLevelSO, string>.GetAccessor("_levelID");
+
+		public void UpdateFakeBeatmap(int lengthSeconds) {
+			if(lengthSeconds > 120 * 60)
+				throw new ArgumentOutOfRangeException();
+
+			// for ref access
+			var beatmapLevel = Anlasser.beatmapLevel;
+
+			var isFirst = beatmapLevel.songAudioClip == null;
+
+			if(lastShaffuruMapLength != lengthSeconds) {
+				if(beatmapLevel.songAudioClip != null)
+					GameObject.Destroy(beatmapLevel.songAudioClip);
+
+				var audioClip = AudioClip.Create("", lengthSeconds * 1000, 1, 1000, false);
+
+				BeatmapLevelSO_audioClip(ref beatmapLevel) = audioClip;
+				lastShaffuruMapLength = lengthSeconds;
+			}
+
+			const float bpm = 13.37f;
+
 			var comedy = (DateTime.Now.Day == 1 && DateTime.Now.Month == 4) || rngSource.Value.NextDouble() >= 0.98;
 			var a = (bpm * ((lengthSeconds - 1) / 60f)).ToString("0.000", System.Globalization.CultureInfo.InvariantCulture);
 
@@ -60,29 +80,37 @@ namespace Shaffuru.MenuLogic {
 				beatmapLevelData.SetJsonData(@"{""_version"":""2.2.0"",""_events"":[{""_value"":3}],""_notes"":[{""_time"":0.5,""_lineIndex"":1,""_cutDirection"":1},{""_time"":0.5,""_lineIndex"":1,""_lineLayer"":1,""_cutDirection"":1},{""_time"":0.5,""_lineIndex"":1,""_lineLayer"":2,""_cutDirection"":1},{""_time"":0.5,""_lineIndex"":2,""_type"":1,""_cutDirection"":8},{""_time"":" + a + @"}],""_obstacles"":[{""_time"":0.4,""_lineIndex"":3,""_duration"":3,""_width"":1},{""_time"":0.4,""_duration"":3,""_width"":1},{""_time"":" + a + @",""_duration"":0.1,""_width"":4}],""_waypoints"":[]}");
 			}
 
-			beatmapLevel.InitFull(
-				LevelId,
-				comedy ? "Susfuru" : "Shaffuru",
-				"",
-				"Kinsi55",
-				"Kinsi55",
-				audioClip,
-				bpm,
-				0,
-				0,
-				0,
-				0,
-				0,
-				SongCore.Loader.defaultCoverImage,
-				defaultEnvironment,
-				defaultEnvironment,
-				new[] { new BeatmapLevelSO.DifficultyBeatmapSet(standardCharacteristic, new[] { difficultyBeatmap }) }
-			);
+			if(isFirst) {
+				beatmapLevel.InitFull(
+					LevelIdPrefix,
+					"Shaffuru",
+					"",
+					"Kinsi55",
+					"Kinsi55",
+					beatmapLevel.songAudioClip,
+					bpm,
+					0,
+					0,
+					0,
+					0,
+					0,
+					SongCore.Loader.defaultCoverImage,
+					defaultEnvironment,
+					defaultEnvironment,
+					new[] { new BeatmapLevelSO.DifficultyBeatmapSet(standardCharacteristic, new[] { difficultyBeatmap }) }
+				);
+			}
 
-			//var x = new GameplayModifiers(false, false, GameplayModifiers.EnergyType.Bar, true, false, false, GameplayModifiers.EnabledObstacleType.All,
-			//	false, false, false, false, GameplayModifiers.SongSpeed.Normal, false, false, false, false, false);
+			BeatmapLevelSO_songName(ref beatmapLevel) = $"Shaffuru ({lengthSeconds / 60} Minutes)";
+			BeatmapLevelSO_levelID(ref beatmapLevel) = $"{LevelIdPrefix}{lengthSeconds}";
+		}
+		
 
-			//var l =
+		public void Start(int lengthSeconds, int rngSeed = 0) {
+			if(rngSeed != 0)
+				rngSource.Value = new System.Random(rngSeed);
+
+			UpdateFakeBeatmap(lengthSeconds);
 
 			menuTransitionsHelper.StartStandardLevel(
 				"Shaffuru",
